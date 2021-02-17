8332cd95f898a6163fbe0f25e9a705cb8a447626
Shiori Uchiyama <sc08.g12.u.s.05.k@gmail.com>
2021-02-17 16:09:53 +0900

    利用規約リンク追加

--- src/components/organisms/sections/UiDefaultFooter.vue
+++ src/components/organisms/sections/UiDefaultFooter.vue
@@ -5,6 +5,9 @@
         v-col(:cols="12")
           p.caption.mb-0.grey--text.text--lighten-2.text-center
             | © 2020. All Rights Reserved.
+        v-col(:cols="12") 
+          v-btn(href="./../../YTシークレットサーチ利用規約.pdf" target="_blank")
+             | 利用規約
 </template>
 
 <script lang="ts">
